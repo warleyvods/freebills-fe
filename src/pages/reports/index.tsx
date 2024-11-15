7749cd84bbe83f4dd3a20d7b428bb437455c7014
Warley <warleyvods@gmail.com>
2024-11-15 19:43:32 -0300

    melhoria no layout

--- src/pages/reports/index.tsx
+++ src/pages/reports/index.tsx
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 import { useDashboardExpenseGraph } from "../../hooks/dashboard/useDashboardExpenseGraph";
 import PieChart from "../../components/PieChart";
 import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import IconComponent from "../../components/Icons/IconComponent";
 
 export default function ReportPage() {
   const [currentDate, setCurrentDate] = useState(new Date());
@@ -30,16 +31,19 @@ export default function ReportPage() {
 
   const formatDate = () => {
     if (showMonth) {
-      const monthName = currentDate.toLocaleString("default", { month: "long" });
+      const monthName = currentDate.toLocaleString("default", {month: "long"});
       return `${monthName.charAt(0).toUpperCase()}${monthName.slice(1)} ${currentDate.getFullYear()}`;
     }
     return `${currentDate.getFullYear()}`;
   };
 
-  const selectedMonth = showMonth ? currentDate.getMonth() + 1 : null; // Meses são baseados em 0
+  const selectedMonth = showMonth ? currentDate.getMonth() + 1 : null;
   const selectedYear = currentDate.getFullYear();
 
-  const { data: expenseDash, isLoading: isLoadingExpenseDash } = useDashboardExpenseGraph(null, selectedMonth, selectedYear);
+  const {
+    data: expenseDash,
+    isLoading: isLoadingExpenseDash
+  } = useDashboardExpenseGraph(null, selectedMonth, selectedYear);
 
   // Chamadas de useColorModeValue
   const tableBg = useColorModeValue("gray.50", "gray.700");
@@ -49,18 +53,159 @@ export default function ReportPage() {
   const secondaryTextColor = useColorModeValue("gray.500", "gray.400");
 
   if (isLoadingExpenseDash) {
-    return <Spinner />;
+    return (
+      <SideBarLayout>
+        <HeadingTable title={"Relatórios"} />
+        {/* Seletor de mês e ano */}
+        <Flex direction="column" align="center" gap="4" mt="4">
+          <Flex align="center" gap="4">
+            <IconButton
+              _focus={{boxShadow: "none"}}
+              onClick={handlePrevious}
+              size="sm"
+              variant="ghost"
+              aria-label={"show info"}
+              icon={<ChevronLeftIcon w={8} h={8} />}
+            />
+            <Text fontSize="lg" fontWeight="medium">
+              {formatDate()}
+            </Text>
+            <IconButton
+              _focus={{boxShadow: "none"}}
+              onClick={handleNext}
+              size="sm"
+              variant="ghost"
+              aria-label={"show info"}
+              icon={<ChevronRightIcon w={8} h={8} />}
+            />
+          </Flex>
+          <Flex align="center" gap="2">
+            <Text>Exibir mês:</Text>
+            <Switch size={"sm"} isChecked={showMonth} onChange={toggleShowMonth} />
+          </Flex>
+        </Flex>
+
+        {/* Gráfico de despesas e tabela */}
+        <Flex
+          mt={"15px"}
+          w={"full"}
+          h={"full"}
+          border={"1px"}
+          p={"20px"}
+          borderRadius={"10px"}
+          borderColor={borderColor}
+          direction={["column", "row"]}
+          align={["center", "center"]}
+          justify={["center", "center"]}
+        >
+          <Flex
+            w={"full"}
+            maxW={"1000px"}
+            direction={["column", "row"]}
+            align={["center", "center"]}
+            justify={["center", "center"]}
+            gap={"5px"}
+          >
+            {/* Gráfico de Pizza */}
+            <Box
+              h={"300px"}
+              w={"300px"}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Flex direction={"column"} justify={"center"} alignItems={"center"} gap={"20px"}>
+                <Text fontSize={"18px"}>Carregando...</Text>
+                <Spinner size={["xl"]} />
+              </Flex>
+            </Box>
+          </Flex>
+        </Flex>
+      </SideBarLayout>
+    );
+  }
+
+  if (expenseDash?.series.length == 0) {
+    return (
+      <SideBarLayout>
+        <HeadingTable title={"Relatórios"} />
+        {/* Seletor de mês e ano */}
+        <Flex direction="column" align="center" gap="4" mt="4">
+          <Flex align="center" gap="4">
+            <IconButton
+              _focus={{boxShadow: "none"}}
+              onClick={handlePrevious}
+              size="sm"
+              variant="ghost"
+              aria-label={"show info"}
+              icon={<ChevronLeftIcon w={8} h={8} />}
+            />
+            <Text fontSize="lg" fontWeight="medium">
+              {formatDate()}
+            </Text>
+            <IconButton
+              _focus={{boxShadow: "none"}}
+              onClick={handleNext}
+              size="sm"
+              variant="ghost"
+              aria-label={"show info"}
+              icon={<ChevronRightIcon w={8} h={8} />}
+            />
+          </Flex>
+          <Flex align="center" gap="2">
+            <Text>Exibir mês:</Text>
+            <Switch size={"sm"} isChecked={showMonth} onChange={toggleShowMonth} />
+          </Flex>
+        </Flex>
+
+        {/* Gráfico de despesas e tabela */}
+        <Flex
+          mt={"15px"}
+          w={"full"}
+          h={"full"}
+          border={"1px"}
+          p={"20px"}
+          borderRadius={"10px"}
+          borderColor={borderColor}
+          direction={["column", "row"]}
+          align={["center", "center"]}
+          justify={["center", "center"]}
+        >
+          <Flex
+            w={"full"}
+            maxW={"1000px"}
+            direction={["column", "row"]}
+            align={["center", "center"]}
+            justify={["center", "center"]}
+            gap={"5px"}
+          >
+            {/* Gráfico de Pizza */}
+            <Box
+              h={"300px"}
+              w={"300px"}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Flex direction={"column"} justify={"center"} alignItems={"center"} gap={"20px"}>
+                <Text fontSize={"18px"}>Não dá dados para exibir</Text>
+                <IconComponent name={"noData2"} width={"150"} height={"150"}/>
+              </Flex>
+            </Box>
+          </Flex>
+        </Flex>
+      </SideBarLayout>
+    );
   }
 
   return (
     <SideBarLayout>
       <HeadingTable title={"Relatórios"} />
-
       {/* Seletor de mês e ano */}
       <Flex direction="column" align="center" gap="4" mt="4">
         <Flex align="center" gap="4">
           <IconButton
-            _focus={{ boxShadow: "none" }}
+            _focus={{boxShadow: "none"}}
             onClick={handlePrevious}
             size="sm"
             variant="ghost"
@@ -71,7 +216,7 @@ export default function ReportPage() {
             {formatDate()}
           </Text>
           <IconButton
-            _focus={{ boxShadow: "none" }}
+            _focus={{boxShadow: "none"}}
             onClick={handleNext}
             size="sm"
             variant="ghost"
@@ -81,12 +226,13 @@ export default function ReportPage() {
         </Flex>
         <Flex align="center" gap="2">
           <Text>Exibir mês:</Text>
-          <Switch isChecked={showMonth} onChange={toggleShowMonth} />
+          <Switch size={"sm"} isChecked={showMonth} onChange={toggleShowMonth} />
         </Flex>
       </Flex>
 
       {/* Gráfico de despesas e tabela */}
       <Flex
+        mt={"15px"}
         w={"full"}
         h={"full"}
         border={"1px"}
